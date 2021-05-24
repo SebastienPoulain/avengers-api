9ddbe6150c32e711e02ab5f8756db9558e1a6017
seb <sebastienpoulain@hotmail.ca>
2021-05-23 21:06:35 -0400

    supprimer elements inutiles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
     skip_before_action :verify_authenticity_token
-    helper_method :login!, :logged_in?, :current_user, :authorized_user?, :logout!
+    helper_method :login!, :logged_in?, :current_user, :logout!
   def login!
       session[:user_id] = @user.id
     end
@@ -10,9 +10,6 @@ class ApplicationController < ActionController::Base
   def current_user
       @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
-  def authorized_user?
-       @user == current_user
-     end
   def logout!
        session.clear
      end
